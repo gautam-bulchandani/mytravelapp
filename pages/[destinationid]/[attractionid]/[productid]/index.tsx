0b0492e8f2a3506b4242b669d0f7f1805c0f42b9
NAMRATA KUMARI <50000195@mahindra.com>
2023-04-01 16:55:34 +0530

    Product detail

--- pages/[destinationid]/[attractionid]/[productid]/index.tsx
+++ pages/[destinationid]/[attractionid]/[productid]/index.tsx
@@ -1,8 +1,46 @@
 import { GetProductDetails } from "@/helper/propshelper";
 import Activity from "@/models/activity";
+import Image from "next/image";
 
 const Activity: React.FC<Activity> = (props) => {
-  return <h1>{props.name}</h1>;
+  console.log(props)
+  return (
+    <section id="service-details" className="service-details">
+      <div
+        className="container"        
+      >
+        <div className="row gy-4">
+          <div className="col-lg-4">
+            <div className="services-list">
+              <div className="cost-package">Package Cost: ${props.price}</div>
+              <div className="stars">
+                <span>Rating: </span>
+              </div>
+            </div>
+            {/* Form Component  */}
+          </div>
+
+          <div className="col-lg-8">
+            <Image
+              src="/assets/img/m_Beaches_8_l_405_630.avif"
+              height={200}
+              width={200}
+              alt=""
+              className="img-fluid services-img"
+            />
+            <h3>Overview</h3>
+            <p>{props.shortdescription}</p>
+            <h3>Tips</h3>
+
+            <h3>What To Expect?</h3>
+            <p>
+             {props.description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Activity;
 export async function getServerSideProps(context: any) {
